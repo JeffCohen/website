f6c5d478c93128770eec75ebdb375cc48de54aa7
Jeff Cohen <cohen.jeff@gmail.com>
2017-05-07 17:46:51 -0500

    more logos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
 
   def client_logos
     logos = %w(bloomberg.jpg scaledown.png enova.png
-          groupon.png juniper.jpg pch.png blue_magnet.png chicago_run.jpg )
+          groupon.png zoku.svg ama.png juniper.jpg pch.png
+          inventables.png blue_magnet.png chicago_run.jpg )
     logos.map { |logo| "clients/#{logo}" }
   end
 end
