7b059474322b036c71a6f4ebac9b42e915492ec7
Jeff Cohen <cohen.jeff@gmail.com>
2017-05-07 21:38:08 -0500

    rethink home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
   def client_logos
-    logos = %w(bloomberg.jpg scaledown.png enova.png
-          groupon.png zoku.svg ama.png juniper.jpg pch.png
+    logos = %w(bloomberg.jpg scaledown.png nu.png enova.png
+          groupon.png uchicago.gif zoku.svg ama.png juniper.jpg pch.png
           inventables.png ilboe.png blue_magnet.png chicago_run.jpg )
     logos.map { |logo| "clients/#{logo}" }
   end
