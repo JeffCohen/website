a4d25a717b10209d4ea336c8cffc3f8bcf6de64e
Jeff Cohen <cohen.jeff@gmail.com>
2017-06-01 21:42:21 -0500

    wyzant

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def client_logos
-    logos = %w(bloomberg.jpg scaledown.png nu.png enova.png
+    logos = %w(bloomberg.jpg wyzant.jpg scaledown.png nu.png enova.png
           groupon.png uchicago.gif zoku.svg ama.png juniper.jpg pch.png
           inventables.png ilboe.png blue_magnet.png chicago_run.jpg )
     logos.map { |logo| "clients/#{logo}" }
