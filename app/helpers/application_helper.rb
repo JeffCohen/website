ae1266230e888e290563cf90365cb8c638117e6b
Jeff Cohen <cohen.jeff@gmail.com>
2017-06-23 23:25:46 -0500

    8bit logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def client_logos
     logos = %w(bloomberg.jpg wyzant.jpg scaledown.png nu.png enova.png
           groupon.png uchicago.gif zoku.svg ama.png juniper.jpg pch.png
-          inventables.png ilboe.png blue_magnet.png chicago_run.jpg )
+          inventables.png ilboe.png blue_magnet.png 8bit.png  chicago_run.jpg )
     logos.map { |logo| "clients/#{logo}" }
   end
 end
